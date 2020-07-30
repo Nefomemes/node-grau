9cffd116c2652922ffcd76a138bd119f8c4a3c62
Nefomemes <66000635+Nefomemes@users.noreply.github.com>
2020-07-30 13:57:42 +0700

    Finished the documentation.

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
 # node-grau
+A framework for MongoDB Atlas dedicated to Discord bot development. Used by Nefomemes/Kylebot for database purposes.
+
+## Installing the dependency
+This dependency is not yet published to Node Package Manager (NPM). Perhaps soon when Nefomemes/Kylebot goes beta.
+
+## Using the framework
+It's simple, as it's a framework ofc.
+
+Before you use this framework, you should have a connection URL of a MongoDB Atlas cluster (or whatever connection url the MongoDB Node.js driver supports).
+
+Because it has a password in it. It is recommended to put the connection url in an `.env` file.
+
+You may want to use the [`dotenv`](https://www.npmjs.com/package/dotenv) dependency if your hosting provider doesn't have an environment variable setup (or you simply uses localhost).
+
+Here we just give a quick code to show you how easy is to use this framework. The code below won't work though because the `url` variable have not been defined.
+
+```js
+const grau = require("node-grau");
+const db = new grau(url, 'main')
+
+console.log(JSON.stringify(db.getDoc('users', '665419057075585025')))
+```
+
+## Documentation
+
+### grau()
+----------
+Is a contructor ofc, this is the setup function you need to get started.
+
+#### Parameters
+| Parameter | Description |
+| ----------- | ----------- |
+| `url` | The connection URL for your MongoDB database, most likely Atlas. |
+| `dbName` | The name of the database you want. |
+
+#### Example
+```js
+const db = new grau(process.env.DB, 'main')
+```
+
+#### Properties
+- client
+
+#### Methods
+
+- getDoc()
+- updateDoc()
+
+#### Properties
+
+**`client`**
+
+The database client.
+
+#### Methods
+
+**`getDoc()`**
+
+Get only a single document. If there is no document with that ID, it will be created.
+
+##### Parameters
+| Parameter | Description | Type |
+| ----------- | ----------- | ---- |
+| `collection` | The collection you want too search the document in. | String |
+| `docID` | The main ID of the document, different than the `_id` property every document have. Let's say the user ID, guild ID, or messageID. | Snowflake |
+
+##### Example
+```js
+const grau = require("node-grau");
+const db = new grau(url, 'main')
+
+console.log(JSON.stringify(db.getDoc('users', '665419057075585025')))
+```
+
+##### Returns
+Object (the document obv)
+
+**updateDoc()**
+
+Update only a single document. If there is no document with that ID, it will be created.
+
+##### Parameters
+| Parameter | Description | Type |
+| ----------- | ----------- | ---- |
+| `collection` | The collection you want too search the document in. | String |
+| `docID` | The main ID of the document, different than the `_id` property every document have. Let's say the user ID, guild ID, or messageID. | Snowflake |
+| `operation` | Basically the operation you want to do, like `{$set: value}` or etc. You shouldn't add the `$currentDate` operator as the framework automatically sets it to `true`. | Object |
+
+##### Example
+```js
+const grau = require("node-grau");
+const db = new grau(url, 'main')
+
+console.log(JSON.stringify(db.updateDoc('users', '665419057075585025', {$set: {playercard: "grau_damascus"}})))
+```
+
+##### Returns
+Object (the updated document).
--- database.js
+++ database.js
@@ -1,52 +1,53 @@
-function main(url, dbName){
-(async function () {
-    const { MongoClient } = require("mongodb");
-    const assert = require("assert");
+function main(url, dbName) {
+    (async function () {
+        const { MongoClient } = require("mongodb");
+        const assert = require("assert");
 
-    MongoClient.connect(url, function (err, dbClient) {
-        assert.equal(null, err);
-        this.client = dbClient;
-        db = dbClient.db(dbName);
-        function getDoc(collection, docID) {
-            return new Promise(async (resolve, reject) => {
-                try {
-                    (async function () {
-                        if(await !collection || collection.constructor !== String)reject("Not a valid collection!");
-                        if (await !docID || Number.isNaN(parseInt(docID)) || docID.constructor !== String) reject("Not a snowflake!");
-                        var doc = await db.collection(collection).findOne({ docID: docID });
+        MongoClient.connect(url, function (err, dbClient) {
+            assert.equal(null, err);
+            this.client = dbClient;
+            db = dbClient.db(dbName);
+            function getDoc(collection, docID) {
+                return new Promise(async (resolve, reject) => {
+                    try {
+                        (async function () {
+                            if (await !collection || collection.constructor !== String) reject("Not a valid collection!");
+                            if (await !docID || Number.isNaN(parseInt(docID)) || docID.constructor !== String) reject("Not a snowflake!");
+                            var doc = await db.collection(collection).findOne({ docID: docID });
 
-                        if (!doc) {
-                            await db.collection(collection).insertOne({ docID: docID })
-                            resolve(await db.collection(collection).findOne({ docID: docID }))
-                        } else {
-                            resolve(doc);
-                        }
-                    })()
-                } catch (e) {
-                    reject(e);
-                }
-            })
-        }
+                            if (!doc) {
+                                await db.collection(collection).insertOne({ docID: docID })
+                                resolve(await db.collection(collection).findOne({ docID: docID }))
+                            } else {
+                                resolve(doc);
+                            }
+                        })()
+                    } catch (e) {
+                        reject(e);
+                    }
+                })
+            }
 
-        function updateDoc(collection, docID, status) {
-            return new Promise(async (resolve, reject) => {
-                try {
-                    (async function () {
-                        if(await !collection || collection.constructor !== String)reject("Not a valid collection!");
-                        if (await !status || status.constructor !== Object) reject("Not an object!");
-                        if (await !docID || Number.isNaN(parseInt(docID)) || docID.constructor !== String) reject("Not a snowflake!");
-                        await getDoc(docID);
-                        await db.collection(collection).updateOne({ docID: docID }, {status, $currentDate: { lastModified: true }})
-                        resolve(await getDoc(docID));
-                    })()
-                } catch (e) {
-                    reject(e);
-                }
-            })
-        }
-        this.updateDoc = updateDoc;
-        this.getDoc = getDoc;
-    });
-})();
+            function updateDoc(collection, docID, operation) {
+                return new Promise(async (resolve, reject) => {
+                    try {
+                        (async function () {
+                            if (await !collection || collection.constructor !== String) reject("An invalid collection was provided.");
+                            if (await !operation || operation.constructor !== Object) reject("`operation` is not an object!");
+                            if (await !docID || Number.isNaN(parseInt(docID)) || docID.constructor !== String) reject("Not a snowflake!");
+                            await delete operation.$currentDate;
+                            await getDoc(docID);
+                            await db.collection(collection).updateOne({ docID: docID }, { ...operation, $currentDate: { lastModified: true } })
+                            resolve(await getDoc(docID));
+                        })()
+                    } catch (e) {
+                        reject(e);
+                    }
+                })
+            }
+            this.updateDoc = updateDoc;
+            this.getDoc = getDoc;
+        });
+    })();
 }
 module.exports = main;
