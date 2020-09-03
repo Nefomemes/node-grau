c4d7d01d96f0ebc00486def30d72cbcd10a5e438
Nefomemes <66000635+Nefomemes@users.noreply.github.com>
2020-09-03 16:15:42 +0700

    Update client.js

--- client.js
+++ client.js
@@ -1,1 +1,17 @@
 
+const supported =  [String, Object, Number];
+function main(url, dbName) {
+    const mod = this;
+    (async function () {
+        const { MongoClient } = require("mongodb");
+        const assert = require("assert");
+
+        MongoClient.connect(url, function (err, dbClient) {
+            assert.equal(null, err);
+            mod.client = dbClient;
+            db = dbClient.db(dbName);
+            
+        });
+    })();
+}
+module.exports = main;
