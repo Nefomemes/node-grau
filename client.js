41c08f1fb42444328abe049cf31c1e738098ef82
Nefomemes <66000635+Nefomemes@users.noreply.github.com>
2020-09-04 08:41:23 +0700

    Update client.js

--- client.js
+++ client.js
@@ -1,16 +1,9 @@
 
-const supported =  [String, Object, Number];
-   const { MongoClient } = require("mongodb")
+
+const { MongoClient } = require("mongodb")
 class GrauClient extends MongoClient {
-    const mod = this;
     (async function () {
  await super.connect();
-          
-              
-   
-            
-           
-           
     })();
 }
 module.exports = GrauClient;
